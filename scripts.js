58f3dc9e011eca9210dccfa45e97620d7af28377
Fellipe Neves <fellipe2.0e@gmail.com>
2025-04-15 21:35:53 -0300

    feature: started logic to create new expense

--- scripts.js
+++ scripts.js
@@ -1,11 +1,7 @@
-const expenseInput = document.getElementById("expense");
+const expense = document.getElementById("expense");
 const amount = document.getElementById("amount");
-
-amount.oninput = () => {
-    let value = amount.value.replace(/\D/g, "");
-    value = Number(value)/100;
-    amount.value = formatCurrencyBRL(value);
-}
+const category = document.getElementById("category");
+const form = document.querySelector("form");
 
 function formatCurrencyBRL(value){
     value = value.toLocaleString("pt-BR", {
@@ -14,4 +10,26 @@ function formatCurrencyBRL(value){
     });
 
     return value;
+}
+
+amount.oninput = () => {
+    let value = amount.value.replace(/\D/g, "");
+    value = Number(value)/100;
+    amount.value = formatCurrencyBRL(value);
+}
+
+
+form.onsubmit = (event) => {
+    event.preventDefault();
+
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at: new Date(),
+    }
+
+    console.log(newExpense);
 }
